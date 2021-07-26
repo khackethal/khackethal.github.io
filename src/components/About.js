dfdc8d5a1a85290984e4fe2b852a56cd3771fb1e
kath <khackethal.freelance@gmail.com>
2021-07-26 19:51:40 +0100

    more about me section added

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,39 @@ function About () {
 
   const [ extraAboutOpen, setExtraAboutOpen ] = React.useState(false)
 
-  // const handleOpenExtraAbout = () => {
-  //   setExtraAboutOpen(!extraAboutOpen)
-  // }
+  const handleOpenExtraAbout = () => {
+    setExtraAboutOpen(!extraAboutOpen)
+  }
 
   const handleCloseExtraAbout = () => {
     setExtraAboutOpen(!extraAboutOpen)
+    setShowGames(false)
+    setShowMusic(false)
+    setShowCurrentlyReading(false)
+    setShowCurrentlyLearning(false)
+  }
+
+  //extra about section state toggles
+  const [ showMusic, setShowMusic ] = React.useState(false)
+  const [ showGames, setShowGames ] = React.useState(false)
+  const [ showCurrentlyReading, setShowCurrentlyReading ] = React.useState(false)
+  const [ showCurrentlyLearning, setShowCurrentlyLearning ] = React.useState(false)
+
+
+  const handleShowGames = () => {
+    setShowGames(!showGames)
+  }
+
+  const handleShowMusic = () => {
+    setShowMusic(!showMusic)
+  }
+
+  const handleShowCurrentlyReading = () => {
+    setShowCurrentlyReading(!showCurrentlyReading)
+  }
+  
+  const handleShowCurrentlyLearning = () => {
+    setShowCurrentlyLearning(!showCurrentlyLearning)
   }
 
   return (
@@ -31,7 +58,7 @@ function About () {
           My very first job was at an e-commerce startup, <br></br> where I was involved in shoot production but also very close to the software engineering team, which helped me to develop a good understanding of the Frontend and Backend setup of the website. <p>That job started my interest in coding, leading to several courses taken in my spare time, and finally the decision to switch to Software Engineering full time.
           I’m curious to learn, I love a good puzzle and finding solutions. </p> <p>I have great experience working in and managing teams, and an excellent understanding of other job roles and the lifecycle of delivering a product to clients. 
             </p><p>I’m currently looking for a position as Junior Software Engineer, ideally working with React. </p>
-            {/* <button onClick={handleOpenExtraAbout} id="contact-button-2" className="more-about-button">More</button> */}
+            <button onClick={handleOpenExtraAbout} id="contact-button-2" className="more-about-button">More</button>
             <div className="portrait-div">
               <img  id="about-portrait" width="90px" src="https://i.imgur.com/pwie0Y7.png" alt="linkedin logo"/>
             </div>
@@ -81,38 +108,71 @@ function About () {
   <h1  id="about-section-header"></h1>
 
   <div className="inner-about-section">
-    <div className="left-side-about">
+    <div className="more-about-div">
       <div>
-        <h2 id="about-card-title-black">More</h2>
+        <h2 id="about-card-title-black">More about me</h2>
         
-        <div>
-          Animal
+        <div className="more-about-inner-div">
+
+          { !showMusic && !showGames && !showCurrentlyLearning && !showCurrentlyReading &&
+            <button className="more-about-div-button-music" onClick={handleShowMusic}><div id="button-text"> </div></button>}
+          { showMusic && 
+            <div className="more-about-text-div">
+              <h2 id="more-about-card-title-black">Instruments I play</h2>
+              <div className="inner-about">
+                <p>Guitar - Fairly well</p>
+                <p>Drums - Badly, but with confidence</p>
+                <button className="more-about-div-button" onClick={handleShowMusic}>x</button>
+              </div>
+            </div>
+          }
+
+          { !showMusic && !showGames && !showCurrentlyLearning && !showCurrentlyReading && 
+          <button className="more-about-div-button-games" onClick={handleShowGames}> <div id="button-text"></div></button>}
+          { showGames && 
+            <div className="more-about-text-div">
+              <h2 id="more-about-card-title-black">Favourite Vintage Games</h2>
+              <div className="inner-about">
+                <p>Analog: Mensch Ärgere Dich Nicht</p>
+                <p><a id="external-link" href="https://en.wikipedia.org/wiki/Mensch_%C3%A4rgere_Dich_nicht" target="blank">{' =>  Read about it'}</a></p>
+                <p><a id="external-link" href="https://en.wikipedia.org/wiki/Bomberman_Online" target="blank">Digital: Bomberman</a></p>
+                <p><a id="external-link" href="https://stadia.google.com/games/super-bomberman-r-online" target="blank">{' => Play it online'}</a></p>
+                <button className="more-about-div-button" onClick={handleShowGames}>x</button>
+              </div>
+            </div>
+          }
+
+          { !showMusic && !showGames && !showCurrentlyLearning && !showCurrentlyReading && 
+            <button className="more-about-div-button-reading" onClick={handleShowCurrentlyReading}><div id="button-text"></div></button>}
+          { showCurrentlyReading && 
+            <div className="more-about-text-div">
+              <h2 id="more-about-card-title-black">Currently Reading</h2>
+              <div className="inner-about">
+                <p><a id="external-link" href="https://www.amazon.co.uk/Overstory-Shortlisted-Booker-Prize-2018/dp/1784708240" target="blank">The Overstory - Richard Powers</a></p>
+                <p><a id="external-link" href="https://www.penguinrandomhouse.com/books/612693/death-in-her-hands-by-ottessa-moshfegh/" target="blank"> Death in Her Hands - Ottessa Moshfegh </a> </p>
+                <p><a id="external-link" href="https://www.penguin.co.uk/books/178/178533/tubes/9780141049090.html" target="blank"> Tubes - Andrew Blum </a> </p>
+                <button className="more-about-div-button" onClick={handleShowCurrentlyReading}>x</button>
+              </div>
+            </div>
+          }
+
+          { !showMusic && !showGames && !showCurrentlyLearning && !showCurrentlyReading && 
+          <button className="more-about-div-button-learning" onClick={handleShowCurrentlyLearning}><div id="button-text"> </div></button>}
+          { showCurrentlyLearning && 
+            <div className="more-about-text-div">
+              <h2 id="more-about-card-title-black">Currently Learning</h2>
+              <div className="inner-about">
+                <p>Next.js</p>
+                <p>Web GL</p>
+                <button className="more-about-div-button" onClick={handleShowCurrentlyLearning}>x</button>
+              </div>
+            </div>
+          }
+          <button onClick={handleCloseExtraAbout} id="less-button" className="more-about-button">Less</button>
         </div>
-
-        <div>
-          Games
-        </div>
-
-        <div>
-        Travel
-        </div>
-
-        <div>
-          Leisure
-        </div>
-
-      
-
-      
-
-
-        <button onClick={handleCloseExtraAbout} id="contact-button-2" className="more-about-button">Less</button>
-      </div>
-      
+      </div>     
     </div>
-
   </div>
-
 </section>            
       }
     </>
